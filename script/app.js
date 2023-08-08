e2b8e82a1b5e5e9c8cdbf57c771d3ef8d3a187e7
Artur Novikau <111690337+ArturNovikov@users.noreply.github.com>
2023-08-08 16:58:11 +0300

    Add files via upload

---
+++ script/app.js
@@ -0,0 +1,48 @@
+const searchInput = document.getElementById('searchInput');
+const autocompleteList = document.getElementById('autocompleteList');
+const repositoryList = document.getElementById('repositoryList');
+
+// Функция debounce
+function debounce(func, delay) {
+    let timeout;
+    return function(...args) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(this, args), delay);
+    };
+}
+
+// Функция для поиска репозиториев
+function searchRepositories(query) {
+    if (!query) {
+        autocompleteList.style.display = 'none';
+        return;
+    }
+    fetch(`https://api.github.com/search/repositories?q=${query}`)
+        .then(response => response.json())
+        .then(data => {
+            autocompleteList.innerHTML = '';
+            data.items.slice(0, 5).forEach(item => {
+                const div = document.createElement('div');
+                div.textContent = item.name;
+                div.addEventListener('click', () => addRepository(item));
+                autocompleteList.appendChild(div);
+            });
+            autocompleteList.style.display = 'block';
+        });
+}
+
+// Функция для добавления репозитория в список
+function addRepository(repository) {
+    const li = document.createElement('li');
+    li.innerHTML = `Name: ${repository.name}<br>Owner: ${repository.owner.login}<br>Stars: ${repository.stargazers_count}`;
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = '';
+    deleteButton.classList.add('btn-delete');
+    deleteButton.addEventListener('click', () => li.remove());
+    li.appendChild(deleteButton);
+    repositoryList.appendChild(li);
+    searchInput.value = '';
+    autocompleteList.style.display = 'none';
+}
+
+searchInput.addEventListener('keyup', debounce(() => searchRepositories(searchInput.value), 500));
